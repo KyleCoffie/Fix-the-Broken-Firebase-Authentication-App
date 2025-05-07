18f5c33489d43d3cb5bdd145befc6097b1763c91
Kyle Coffie <Kyle.coffie@live.com>
2025-05-07 14:00:49 -0500

    fixed commened problems

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCR1cIQhyFh8G8eDrHcYQ4s46Nq876D_L4",
-  authDomain: "fir-project-d0695.firebaseapp.com",
-  projectId: "fir-project-d0695",
-  storageBucket: "fir-project-d0695.firebasestorage.app",
-  messagingSenderId: "339438867897",
-  appId: "1:339438867897:web:4cd37b3d8cff5097e35268",
-  measurementId: "G-022QYKRK03"
+  apiKey:import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID
 };
-
 // Initialize Firebase (MISSING export for `auth`)
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
